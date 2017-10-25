85a3ba21e8d395f66813006290ac9efbf353059c
BuildTools <unconfigured@null.spigotmc.org>
2017-10-24 21:24:26 -0700

    Initial Commit with gitignore

---
+++ SpiritPointsServer/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace SpiritPointsServer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public enum MessageType { Success, Error, Info, Warning };
+
+        public IActionResult Index()
+        {
+            if(Startup.error != "")
+            {
+                ModelState.AddModelError("Error", Startup.error);
+                Startup.error = "";
+            }
+            return View();
+        }
+
+        //public IActionResult About()
+        //{
+        //    ViewData["Message"] = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public IActionResult Contact()
+        //{
+        //    ViewData["Message"] = "Your contact page.";
+
+        //    return View();
+        //}
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile file, string FullName)
+        {
+            if (file == null || file.Length == 0)
+            {
+                Startup.error = "Please select an Image";
+                return RedirectToAction("");
+            }
+
+            if(FullName == null || FullName == "")
+            {
+                Startup.error = "Please enter your name";
+                return RedirectToAction("");
+            }
+
+            string grade = Request.Form["grade"].First();
+
+            if(grade == null || grade == "select")
+            {
+                Startup.error = "Please select your grade";
+                return RedirectToAction("");
+            }
+
+            //Make sure not duplicate
+            var path = Path.Combine(
+                Directory.GetCurrentDirectory(), 
+                "Pictures", 
+                "ClassOf20" + grade, 
+                FullName.Replace(' ', '_') + "." + Startup.counts[0] + Path.GetExtension(file.FileName));
+
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            Startup.counts[0] = (int.Parse(Startup.counts[0]) + 1).ToString();
+            Startup.WriteCounts();
+
+            return RedirectToAction("");
+        }
+    }
+}
---
+++ SpiritPointsServer/Startup.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+
+namespace SpiritPointsServer
+{
+    public class Startup
+    {
+        public static string[] counts;
+        public static string error = "";
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            CalculateCounts();
+        }
+
+        #region Counts
+        void CalculateCounts()
+        {
+            counts = File.ReadAllLines(Path.Combine(Directory.GetCurrentDirectory(), "Count.txt"));
+        }
+
+        public static void WriteCounts()
+        {
+            File.WriteAllLines(Path.Combine(Directory.GetCurrentDirectory(), "Count.txt"), counts);
+        }
+        #endregion
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddSingleton<IFileProvider>(
+                new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")));
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
